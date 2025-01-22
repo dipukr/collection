0a31cf7c628795bb907965966483722533f9f918
adipukr <dipu.kumar@50hertz.in>
2025-01-22 15:37:32 +0530

    new changes

--- Main.java
+++ Main.java
@@ -1,15 +1,34 @@
-import java.io.File;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.util.*;
-import java.util.stream.IntStream;
+import java.util.stream.Stream;
+import java.util.stream.Collectors;
 
 public class Main {
+	
+	public static int num(int n) {
+		while (n > 26) 
+			n = n / 26;
+		return n;
+	}
+
+	public static char getChar(int n) {
+		return (char)(n + 64);
+	}
+
+	public static String getLabel(int colnum) {
+		colnum += 1;
+		String label = "";
+		int rem = 0;
+		while (colnum > 26) {
+			label += getChar(num(colnum));
+			rem = colnum % 26;
+			colnum /= 26;
+		}
+		label += getChar(rem);
+		return label;
+	}
+	
 	public static void main(final String[] args) throws Exception {
-		Files.lines(Path.of("Main.java"))
-			.forEach(System.out::println);
-		IntStream.of(10, 20, 30, 40, 50)
-			.filter(elem -> elem > 20)
-			.forEach(System.out::println);
+		Stream.iterate(0, i -> i + 1)
+			.limit(1000)
+			.forEach(elem -> System.out.printf("%d:%s\n", elem, getLabel(elem)));
 	}
 }
--- main.c
+++ main.c
@@ -2,10 +2,21 @@
 #include <stdlib.h>
 #include <string.h>
 #include <nonstd.h>
+#define N 11
 
 void main(const int argc, char const **argv)
 {
-	printf("hello.world\n");
+	char a[] = "helloworld";
+	char b[N];
+	memset(b, 0, N);
+	for(int i=0;i<strlen(a);i++) {
+		b[i] = a[i] ^ 4;
+	}
+	printf("%s\n", b);
+	for(int i=0;i<strlen(b);i++) {
+		b[i] = b[i] ^ 4;
+	}
+	printf("%s\n", b);
 }
 
 
