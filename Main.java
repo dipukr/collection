5d544931807051e0b7ede4e46133afe88f4b143a
adipukr <dipu.kumar@50hertz.in>
2025-01-24 17:50:26 +0530

    new changes

--- Main.java
+++ Main.java
@@ -3,32 +3,11 @@ import java.util.stream.Collectors;
 
 public class Main {
 	
-	public static int num(int n) {
-		while (n > 26) 
-			n = n / 26;
-		return n;
-	}
-
-	public static char getChar(int n) {
-		return (char)(n + 64);
-	}
-
-	public static String getLabel(int colnum) {
-		colnum += 1;
-		String label = "";
-		int rem = 0;
-		while (colnum > 26) {
-			label += getChar(num(colnum));
-			rem = colnum % 26;
-			colnum /= 26;
-		}
-		label += getChar(rem);
-		return label;
-	}
+	public static final String RESET = "\u001B[0m";
+	public static final String RED = "\u001B[31m";
 	
-	public static void main(final String[] args) throws Exception {
-		Stream.iterate(0, i -> i + 1)
-			.limit(1000)
-			.forEach(elem -> System.out.printf("%d:%s\n", elem, getLabel(elem)));
+	public static void main(final String[] args) {
+		System.out.printf("%shello_world%s", RED, RESET);
+		System.out.println("hello");
 	}
 }
