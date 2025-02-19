23ded47b39f78677c7f985af3d9a422d135dc453
koomar_dipu <koomar.dipu@gmail.com>
2025-02-20 00:57:26 +0530

    chg

--- main.c
+++ main.c
@@ -7,16 +7,7 @@
 void main(const int argc, char const **argv)
 {
 	char a[] = "helloworld";
-	char b[N];
-	memset(b, 0, N);
-	for(int i=0;i<strlen(a);i++) {
-		b[i] = a[i] ^ 4;
-	}
-	printf("%s\n", b);
-	for(int i=0;i<strlen(b);i++) {
-		b[i] = b[i] ^ 4;
-	}
-	printf("%s\n", b);
+	printf("%s\n", a);
 }
 
 
