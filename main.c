cbab41cf91cbb1fe4220bcaa81a25f04ad26ddeb
adipukr <dipu.kumar@50hertz.in>
2025-08-02 17:20:31 +0530

    chg

--- main.c
+++ main.c
@@ -11,5 +11,17 @@ void main(const int argc, const char **argv)
 	printf("%d\n", v);
 }
 
-
+void merge(int[] data, int[] temp, int low, int mid, int high)
+{
+	int i = low;
+	int j = mid + 1;
+	int k = low;
+	while (i <= mid || j <= high) {
+		if (data[i] < data[j]) {
+			temp[k++] = data[i++];
+		} else {
+			temp[k++] = data[j++];
+		}
+	}   
+}
 
