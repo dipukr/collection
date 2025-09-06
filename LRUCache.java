2b92813c7d88c05f07c5c344f753511803423d13
adipukr <dipu.kumar@50hertz.in>
2025-09-06 16:48:09 +0530

    chg

---
+++ LRUCache.java
@@ -0,0 +1,19 @@
+public class LRUCache {
+	
+	private Map<String, Node> table;
+	private int capacity;
+	private Node head;
+	private Node tail;
+
+	public LRUCache(int capacity) {
+		this.capacity = capacity;
+	}
+
+	public Object get(String key) {
+
+	}
+
+	public void put(String key, Object val) {
+		
+	}
+}
