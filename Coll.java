bf7a7431edeb02f9befebe70472b57ae922135bd
dipukr <adipukr@gmail.com>
2025-10-03 22:05:06 +0530

    chg

--- Coll.java
+++ Coll.java
@@ -1,12 +1,3 @@
-import java.io.File;
-import java.util.Arrays;
-import java.util.Comparator;
-import java.util.Set;
-import java.util.TreeSet;
-
-public class Coll {
-	public static void main(String[] args) throws Exception {
-		Server server = new Server(8780);
-		server.start();
-	}
+void main(String[] args) {
+		System.out.println("hello");
 }
